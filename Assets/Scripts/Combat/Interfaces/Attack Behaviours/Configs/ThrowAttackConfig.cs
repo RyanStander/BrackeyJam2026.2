0ac85a8840861223706577d7b44d13904e351202
Ryan <stander.ryan@gmail.com>
2026-08-26 22:14:30 +0200

    Swatter attack implemented
    fix: $15s

---
+++ Assets/Scripts/Combat/Interfaces/Attack Behaviours/Configs/ThrowAttackConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Combat.Interfaces.Attack_Behaviours.Configs
+{
+    [CreateAssetMenu(fileName = "ThrowAttack", menuName = "Attacks", order = 1)]
+    public class ThrowAttackConfig : ScriptableObject
+    {
+        public int Damage = 25;
+        public float AttackDistance = 5;
+        public float WindupTime = 1;
+        public float ThrowDistance = 7;
+        public float ThrowSpeed = 1;
+        public bool ReturnWeapon = true;
+        public float ReturnSpeed = 1;
+        public float Cooldown = 5;
+    }
+}
