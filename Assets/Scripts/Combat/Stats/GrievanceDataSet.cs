03e44ef4adb42398f9e62c5da14b0f3345382f6b
Ryan <stander.ryan@gmail.com>
2026-08-27 04:49:12 +0200

    Grievance system
    Fix: $13z

---
+++ Assets/Scripts/Combat/Stats/GrievanceDataSet.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Combat.Stats
+{
+    [CreateAssetMenu(fileName = "GrievanceData", menuName = "Grievance", order = 0)]
+    public class GrievanceDataSet : ScriptableObject
+    {
+        [Header("Specific Grievance Values")]
+        public float ExploitGrievance = -3f;
+        public float FailedExploitGrievance = 1f;
+
+        public float ProximityGrievance = 1f;
+    }
+}
