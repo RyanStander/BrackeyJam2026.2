d309f3f69641a52c4e420460d6af92e8a5e505dc
Jacques <jacques.adriaan.venter@gmail.com>
2026-08-28 10:24:53 +0200

    Partial Archive of old Areas Code

---
+++ Assets/Archived/Scripts/Events/EventDataInheritance.cs
@@ -0,0 +1,37 @@
+using Expeditions.Room;
+using UnityEngine;
+
+namespace Events
+{
+    public class CommandAreaExit : EventData
+    {
+        public CommandAreaExit() : base(EventType.CommandAreaExit) { }
+    }
+    public class ExecuteCombatRoom : EventData
+    {
+        public readonly RoomManager SourceRoom;
+        public readonly CombatRoomData RoomData;
+        public readonly Bounds RoomBounds;
+
+        public ExecuteCombatRoom(RoomManager sourceRoom, RoomData roomData, Bounds roomBounds) : base(EventType.ExecuteCombatRoom)
+        {
+            SourceRoom = sourceRoom;
+            RoomData = (CombatRoomData)roomData;
+            RoomBounds = roomBounds;
+        }
+    }
+
+    public class CommandRoomChange : EventData
+    {
+        public readonly RoomManager SourceRoom;
+        public readonly RoomManager TargetRoom;
+        public readonly Vector3 TargetPosition;
+
+        public CommandRoomChange(RoomManager sourceRoom, RoomManager targetRoom, Vector3 targetPosition) : base(EventType.CommandRoomChange)
+        {
+            SourceRoom = sourceRoom;
+            TargetRoom = targetRoom;
+            TargetPosition = targetPosition;
+        }
+    }
+}
--- Assets/Scripts/Events/EventType.cs
+++ Assets/Scripts/Events/EventType.cs
@@ -3,9 +3,9 @@ namespace Events
     //Defines the different event types to be used in event data in enumeration form
     public enum EventType
     {
-        CommandRoomChange,
-        CommandAreaExit,
-        ExecuteCombatRoom,
+        CommandRoomChange, // Archived
+        CommandAreaExit, // Archived
+        ExecuteCombatRoom, // Archived
 
         ReceiveDebug,
 
